a19be0e74cadc7b6e95fc7782a0d2ca38179472e
sagar23sj <sagarsonwane23@gmail.com>
2022-07-24 23:36:10 +0530

    Updated test case for Divide based on BDD

--- main_test.go
+++ main_test.go
@@ -24,18 +24,55 @@ func TestMultiply(t *testing.T) {
 	})
 }
 
-func TestDivision(t *testing.T) {
-	Convey("Divide two numbers", t, func() {
+// func TestDivision(t *testing.T) {
+// 	Convey("Divide two numbers", t, func() {
 
-		Convey("Division by non-zero number", func() {
-			num, err := Division(10, 2)
-			So(err, ShouldBeNil)
-			So(num, ShouldEqual, 5)
+// 		Convey("Division by non-zero number", func() {
+// 			num, err := Division(10, 2)
+// 			So(err, ShouldBeNil)
+// 			So(num, ShouldEqual, 5)
+// 		})
+
+// 		Convey("divide by 0", func() {
+// 			_, err := Division(10, 0)
+// 			So(err, ShouldNotBeNil)
+// 		})
+// 	})
+// }
+
+func TestDivide(t *testing.T) {
+
+	Convey("Scenario: Divide numbers", t, func() {
+
+		Convey("Given Two Integer numbers", func() {
+			num1 := 30
+			num2 := 3
+
+			So(num1, ShouldHaveSameTypeAs, 0)
+			So(num2, ShouldHaveSameTypeAs, num1)
+			Convey("When  numbers are non-zero", func() {
+				So(num1, ShouldNotEqual, 0)
+				So(num2, ShouldNotEqual, 0)
+
+				Convey("Then the result should be an integer value", func() {
+					result, err := Division(num1, num2)
+					So(err, ShouldBeNil)
+					So(result, ShouldEqual, 10)
+				})
+
+			})
+
+			Convey("When numbers can be zero", func() {
+				num2 = 0
+				Convey("Then the result should be error", func() {
+					_, err := Division(num1, num2)
+					So(err, ShouldNotBeNil)
+				})
+
+			})
+
 		})
 
-		Convey("divide by 0", func() {
-			_, err := Division(10, 0)
-			So(err, ShouldNotBeNil)
-		})
 	})
+
 }
